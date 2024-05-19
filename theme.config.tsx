d8d9a34fcc852b7a0007fbce002ef8a0f0cc4182
Owen Bick <12536625+bick@users.noreply.github.com>
2024-05-18 20:09:47 -0400

    Add frameworks

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/bick/placeholderjs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'PlaceholderJS',
   },
 }
 
