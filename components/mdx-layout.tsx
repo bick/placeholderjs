7516101e39df7c35a85776b194676d852499b7b5
Owen Bick <12536625+bick@users.noreply.github.com>
2024-05-24 23:44:42 -0400

    Update docs

--- components/mdx-layout.tsx
+++ components/mdx-layout.tsx
@@ -2,11 +2,19 @@ import Sidebar from "@/components/sidebar";
 
 export default function MdxLayout({children}: { children: React.ReactNode }) {
     return (
-        <div className="flex container">
-            <Sidebar/>
-            <div className="container">
-                {children}
+        <>
+            <div className="block text-xl border-b border-t font-semibold py-3 mb-6">
+                <div className="flex container">
+                    Documentation
+                    <span className="text-[14px] ml-auto opacity-75 font-normal">Last updated: May 24, 2024</span>
+                </div>
             </div>
-        </div>
+            <div className="flex container">
+                <Sidebar/>
+                <div className="flex flex-col">
+                    {children}
+                </div>
+            </div>
+        </>
     )
 }
