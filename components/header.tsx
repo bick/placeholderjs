5845c0f515d3e8a0263cbcb855e7848ca3946702
Owen Bick <12536625+bick@users.noreply.github.com>
2024-05-25 00:35:54 -0400

    Update light mode

--- components/header.tsx
+++ components/header.tsx
@@ -48,7 +48,7 @@ const Header = () => {
     return (
         <header id="nav" className={`header py-6 w-full ${isHomePage ? 'absolute' : ''}`}>
             <div className="container mx-auto flex items-center">
-                <Link href='/' className="logo">
+                <Link href='/' className="logo text-black dark:text-white">
                     PlaceholderJS
                 </Link>
                 <sup>
@@ -57,29 +57,29 @@ const Header = () => {
 
                 <ul className="flex items-center max-md:hidden ml-auto">
                     <li>
-                        <Link href="/docs">
+                        <Link href="/docs" className="text-black dark:text-white">
                             Docs
                         </Link>
                     </li>
                     <li>
-                        <Link href="/#usage">
+                        <Link href="/#usage" className="text-black dark:text-white">
                             Usage
                         </Link>
                     </li>
                     <li>
-                        <Link href="https://github.com/bick/placeholder/releases">
+                        <Link href="https://github.com/bick/placeholder/releases" className="text-black dark:text-white">
                             Changelog
                         </Link>
                     </li>
                     <span aria-hidden="true"
                           className="bg-[rgba(0,0,0,.25)] dark:bg-[rgba(255,255,255,.25)] mx-2 hidden h-5 w-px sm:!inline-block"></span>
                     <li>
-                        <Link href="https://github.com/bick/placeholderjs" className="mx-2" target="_blank">
+                        <Link href="https://github.com/bick/placeholderjs"  className="text-black dark:text-white mx-2" target="_blank" >
                             <FaGithub className="text-xl"/>
                         </Link>
                     </li>
                     <li>
-                        <Link href="https://npmjs.com/placeholder" target="_blank">
+                        <Link href="https://npmjs.com/placeholder" target="_blank" className="text-black dark:text-white">
                             <SiNpm className="text-xl"/>
                         </Link>
                     </li>
