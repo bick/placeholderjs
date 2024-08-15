fc964103404b595851ae0cc528759a0bfea370a2
Owen Bick <12536625+bick@users.noreply.github.com>
2024-08-14 21:41:44 -0500

    Update route.ts

--- app/[placeholder]/route.ts
+++ app/[placeholder]/route.ts
@@ -12,24 +12,36 @@ export async function GET(req: NextRequest, {params}: { params: { placeholder: s
     }
 
     let customText = `${width}x${height}`; // Default text
+    let textColor = '#000000'; // Default text color
+    let backgroundColor = '#cccccc'; // Default background color
 
-    if (req.nextUrl.searchParams.has('text')) {
-        customText = req.nextUrl.searchParams.get('text')?.replace(/\+/g, ' ') || customText;
+    const searchParams = req.nextUrl.searchParams;
+
+    if (searchParams.has('text')) {
+        customText = searchParams.get('text')?.replace(/\+/g, ' ') || customText;
+    }
+
+    if (searchParams.has('color')) {
+        textColor = searchParams.get('color') || textColor;
+    }
+
+    if (searchParams.has('background')) {
+        backgroundColor = searchParams.get('background') || backgroundColor;
     }
 
     const minDimension = Math.min(width, height);
-    const fontSize = minDimension * 0.05; // Font size as 5% of the smaller dimension
+    const fontSize = minDimension * 0.05;
 
     const svg = `
     <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
-      <rect width="100%" height="100%" fill="#cccccc" />
+      <rect width="100%" height="100%" fill="${backgroundColor}" />
       <text 
         x="50%" 
         y="50%" 
         dominant-baseline="middle" 
         text-anchor="middle" 
         font-size="${fontSize}" 
-        fill="#000000"
+        fill="${textColor}"
         font-family='-apple-system, "Inter", sans-serif'>
         ${customText}
       </text>
