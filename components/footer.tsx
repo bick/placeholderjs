247ea2e08cf467fe778e2aac11a9128555e31871
Owen Bick <12536625+bick@users.noreply.github.com>
2024-08-18 22:43:22 -0400

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,10 +9,9 @@ const Footer = () => {
     return (
         <footer className="flex py-24 mt-24 border-t w-full">
             <div className="container flex text-center w-full justify-center">
-                <p className="flex flex-col">
-                    Copyright &copy; 2024 PlaceholderJS. All Rights Reserved. Made with ❤️ and ☕ by&nbsp;
-                    <a href="https://owenbick.com" target="_blank" className="mx-auto">Owen Bick</a>&nbsp;under the MIT license.
-                </p>
+                Made with ❤️ and ☕ by&nbsp;
+                <a href="https://owenbick.com" target="_blank">Owen Bick</a>&nbsp;under the MIT
+                license
             </div>
         </footer>
     );
